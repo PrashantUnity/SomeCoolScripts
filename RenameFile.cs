775cf6aa6efe68030113496a42070febc0e3f75f
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-01-31 15:36:17 +0530

    Update RenameFile.cs

--- RenameFile.cs
+++ RenameFile.cs
@@ -9,31 +9,31 @@ namespace Renamer
 		public static void Main()
 		{
 			Console.WriteLine("Enter File extension format : Like *.mkv,");
-            var format = Console.ReadLine();
+			var format = Console.ReadLine();
 
-            // directory where all files are located Location
-            Console.WriteLine("directory where all files are located Location full path");
-            var dir = Console.ReadLine();
-
-
-
-            // text to be removed
-            Console.WriteLine("Text To Be removed/replaced");
-            var replace = Console.ReadLine();
-
-            Console.WriteLine("Text Place Holder");
-            var placeholder = Console.ReadLine();
+			// directory where all files are located Location
+			Console.WriteLine("directory where all files are located Location full path");
+			var dir = Console.ReadLine();
 
 
-            var allFiles = Directory.GetFiles(dir, $"{format}").Select(Path.GetFileName);
 
-            foreach (var i in allFiles)
-            {
-                if (!File.Exists(Path.Combine(dir, i.Replace($"{replace}", $"{placeholder}"))))
-                {
-                    File.Move(Path.Combine(dir, i), Path.Combine(dir, i.Replace($"{replace}", $"{placeholder}")));
-                }
-            } 
+			// text to be removed
+			Console.WriteLine("Text To Be removed/replaced");
+			var replace = Console.ReadLine();
+
+			Console.WriteLine("Text Place Holder");
+			var placeholder = Console.ReadLine();
+
+
+			var allFiles = Directory.GetFiles(dir, $"{format}").Select(Path.GetFileName);
+
+			foreach (var i in allFiles)
+			{
+				if (!File.Exists(Path.Combine(dir, i.Replace($"{replace}", $"{placeholder}"))))
+				{
+				    File.Move(Path.Combine(dir, i), Path.Combine(dir, i.Replace($"{replace}", $"{placeholder}")));
+				}
+			} 
 		}
 	}
 }
