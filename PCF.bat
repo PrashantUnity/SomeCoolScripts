726beca6383662cf2407d5d5f9d52b31d96c00cf
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-02-27 09:07:59 +0530

    Update PCF.bat

--- PCF.bat
+++ PCF.bat
@@ -1,6 +1,6 @@
 @echo off
 echo ---------------------------------------------------------------------------------
-echo variables name list/ You can name it as per yout requirement
+echo variables name list/ You can name it as per your requirement
 set ParentDir="PCF"
 set AppName="AppName"
 set nameSpaceName="SampleNameSpace"
