d2c4d1074dafb17eec13456919422c7fe8db2f92
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2023-07-09 00:39:55 +0530

    Grouping

--- InstallAppInWindows.ps1
+++ InstallAppInWindows.ps1
@@ -1,10 +1,16 @@
+# Ide , Code Editor , Terminal
 winget install -e --id Git.Git --accept-package-agreements --accept-source-agreements
-winget install -e --id LINQPad.LINQPad.7
 winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Atlassian.Sourcetree
 winget install -e --id Neovim.Neovim
 winget install -e --id Microsoft.VisualStudio.2022.Community
 winget install -e --id Microsoft.SQLServer.2019.Developer
 winget install -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id Unity.UnityHub
+winget install -e --id JetBrains.Rider.EAP
+winget install -e --id JetBrains.CLion.EAP
+winget install -e --id JetBrains.PyCharm.Community
+winget install -e --id LINQPad.LINQPad.7
 winget install -e --id SQLite.SQLite
 winget install -e --id Microsoft.PowerShell.Preview
 winget install -e --id Microsoft.VisualStudioCode
@@ -12,6 +18,9 @@ winget install -e --id Insomnia.Insomnia
 winget install -e --id dbeaver.dbeaver
 winget install -e --id OpenJS.NodeJS
 winget install -e --id Docker.DockerDesktop
+
+# Media and Stuff Must
+winget install -e --id OBSProject.OBSStudio
 winget install -e --id Discord.Discord
 winget install -e --id Mozilla.Firefox
 winget install -e --id Google.Chrome
@@ -19,14 +28,11 @@ winget install -e --id Notepad++.Notepad++
 winget install -e --id Notion.Notion
 winget install -e --id SumatraPDF.SumatraPDF
 winget install -e --id Adobe.Acrobat.Reader.64-bit
-winget install -e --id JanDeDobbeleer.OhMyPosh
 winget install -e --id NGWIN.PicPick
 winget install -e --id Bandisoft.Honeyview
-winget install -e --id Unity.UnityHub
-winget install -e --id JetBrains.Rider.EAP
-winget install -e --id JetBrains.CLion.EAP
-winget install -e --id OBSProject.OBSStudio
 winget install -e --id Microsoft.PCManager
+
+# extra Stuff
 winget install -e --id qBittorrent.qBittorrent
 winget install -e --id Telegram.TelegramDesktop
 winget install -e --id RARLab.WinRAR
@@ -35,5 +41,20 @@ winget install -e --id ProtonTechnologies.ProtonVPN
 winget install -e --id RevoUninstaller.RevoUninstaller
 winget install -e --id GlassWire.GlassWire
 winget install -e --id VideoLAN.VLC
+
+# Personal Choice
+winget install -e --id JanDeDobbeleer.OhMyPosh
 winget install -e --id Valve.Steam
 winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id BlenderFoundation.Blender
+
+# extension install for vs code
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-dotnettools.dotnet-interactive-vscode
+code --install-extension ms-dotnettools.csharp
+code --install-extension hediet.debug-visualizer
+code --install-extension ritwickdey.LiveServer
+code --install-extension ms-vscode.powershell
+code --install-extension esbenp.prettier-vscode
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscode-icons-team.vscode-icons 
