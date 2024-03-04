225aabcfb6dda6aee6f55edb1b5b7cd9711e79fd
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-03-04 08:25:11 +0530

    Create csharp.cs

---
+++ csharp.cs
@@ -0,0 +1,16 @@
+## Enum Get Name
+
+```cs
+using System; 
+
+Console.WriteLine(Enum.GetName(typeof(Language), Language.cs)); 
+public enum Language
+{
+    csharp, 
+    cs,
+    dotnet,
+    hlsl,
+    gdscript,
+    glsl
+}
+```
